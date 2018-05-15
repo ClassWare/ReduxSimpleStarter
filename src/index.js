85a6db930b84528bdff45724d8a1e294e9bc0774
martin <m.bazan@ntlworld.com>
2018-05-15 17:45:55 +0100

    Showing images and first embed Lecture 28

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 // Import
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyAE0-T3jDOKTmvQmPUbQIfJHDeP41zX2RU';
+const API_KEY = 'AIzaSyBMuRx2uE40WhHY9anydvmGArzFwe5qP_k';
 
-const App = () => {
-  return (<div>
-    One
-    <SearchBar></SearchBar>
-    Two
-    <SearchBar></SearchBar>
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos:[] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+      this.setState({ videos: data });
+    });
+
+  }
+  render() {
+    return (<div>
+      NOW WITH ARRAY
+      <SearchBar></SearchBar>
+      <VideoDetail video={this.state.videos[0]}/>
+      <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
 // Take this components generated HTML and insert it into the DOM
